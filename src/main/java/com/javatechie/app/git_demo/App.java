ba548ef95e7ead55fb18689e40427b9d917e3fbf
Gopi SambasivaRao Gopi <samba.sivarao.gopi@gmail.com>
2023-05-25 09:44:26 -0400

    added if condition to check the data

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -4,13 +4,15 @@ package com.javatechie.app.git_demo;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 	public void addData(String data) {
-		System.out.println("Data =" +data);
+		System.out.println("Data =" + data);
+		if(data.equals("java techie")){
+			System.out.println("Welcome");
+		}
 	}
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
